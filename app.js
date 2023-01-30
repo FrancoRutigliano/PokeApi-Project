a11a315f24b47f9a887094ed495739c5197ab6e3
Franco Rutigliano <francorutigliano11@gmail.com>
2023-01-30 12:31:35 -0300

    Visualizando tarjetas de Pokemons en la web

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const poke_container = document.querySelector('#poke_container')
 const pokemons_number = 150
 
 
+
+const ShowPokemons = async () => {
+  for (let i = 1; i <= pokemons_number; i++){
+    await getPokemon(i)
+  }
+}
+
 const getPokemon = async id => {
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`
 
@@ -9,5 +16,21 @@ const getPokemon = async id => {
 
   const pokemon = await res.json()
 
+  createPokemonCard(pokemon)
+
 }
 
+ShowPokemons()
+
+function createPokemonCard(poke) {
+  const PokeEl = document.createElement('div')
+
+  PokeEl.classList.add('pokemon-card')
+
+  
+
+  PokeEl.innerHTML = `${poke.id}`
+
+  poke_container.appendChild(PokeEl)
+
+}
